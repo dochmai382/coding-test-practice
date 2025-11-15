bf56440c2821331cb92b0daf085e2c9341b21379
Dami Cho <dochmai382@gmail.com>
2025-11-15 20:44:40 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2180 ms, Memory: 358464 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -8,64 +8,37 @@ class Solution {
         int T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
             int k = Integer.parseInt(br.readLine());
-            
-            PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-            PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-            Map<Integer, Integer> countMap = new HashMap<>();
-            
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+           
             for (int i = 0; i < k; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 char op = st.nextToken().charAt(0);
                 int num = Integer.parseInt(st.nextToken());
                 
-                switch(op) {
-                    case 'I':
-                        minHeap.offer(num); maxHeap.offer(num);
-                        countMap.put(num, countMap.getOrDefault(num, 0) + 1);
-                        break;
-                    case 'D':
-                        if (countMap.size() == 0) continue;
-                        if (num == 1) { // max
-                            cleanUp(maxHeap, countMap);
-                            if (!maxHeap.isEmpty()) {
-                                int max = maxHeap.poll();
-                                updateCountMap(countMap, max);
-                            }
-                            
-                        } else if (num == -1) { // min
-                            cleanUp(minHeap, countMap);
-                            if (!minHeap.isEmpty()) {
-                                int min = minHeap.poll();
-                                updateCountMap(countMap, min);
-                            }
-                        }
-                        break;
+                if (op == 'I') {
+                    map.put(num, map.getOrDefault(num, 0) + 1);
+                } else if (op == 'D') {
+                    if (map.isEmpty()) continue;
+                    
+                    int target = (num == 1) ? map.lastKey() : map.firstKey();
+                    if (map.get(target) == 1) {
+                        map.remove(target);
+                    } else {
+                        map.put(target, map.get(target) - 1);
+                    }
                 }
             }
             
-            cleanUp(maxHeap, countMap);
-            cleanUp(minHeap, countMap);
-            
-            if (countMap.size() == 0) {
+            if (map.isEmpty()) {
                 sb.append("EMPTY");
             } else {
-                sb.append(maxHeap.peek()).append(" ").append(minHeap.peek());
+                sb.append(map.lastKey()).append(" ").append(map.firstKey());
             }
             
             sb.append("\n");
         } 
     }
     
-    private void cleanUp(PriorityQueue<Integer> heap, Map<Integer, Integer> map) {
-        while (!heap.isEmpty() && map.getOrDefault(heap.peek(), 0) == 0) {
-            heap.poll();
-        }
-    }
-    
-    private void updateCountMap(Map<Integer, Integer> map, int n) {
-        map.computeIfPresent(n, (key, count) -> (count == 1) ? null : count - 1);
-    }
-
 	public String getResult() {
 		return sb.toString().trim();
 	}    
