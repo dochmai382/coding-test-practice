ff933ff289ae86f3a8edb78b5a70973154d3ecfc
Dami Cho <dochmai382@gmail.com>
2025-11-17 17:22:09 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.52 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,11 +1,8 @@
+import java.util.*;
+
 class Solution {
-    public int solution(int[] numbers) {
-        int answer = Integer.MIN_VALUE;
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i+1; j < numbers.length; j++) {
-                answer = Math.max(answer, numbers[i] * numbers[j]);
-            }
-        }
-        return answer;
+    public int solution(int[] numbers) {      
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
     }
 }
