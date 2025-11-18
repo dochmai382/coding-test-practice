f7951340eae1e44f60f6370980412fb02cd7d622
Dami Cho <dochmai382@gmail.com>
2025-11-18 17:20:15 +0900

    [level 0] Title: 문자열 묶기, Time: 25.13 ms, Memory: 128 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] strArr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (String str: strArr) {
+            int length = str.length();
+            map.put(length, map.getOrDefault(length, 0) + 1);
+        }
+        
+        int answer = 0;
+        for (int count: map.values()) {
+            answer = Math.max(answer, count);
+        }
+        return answer;
+    }
+}
