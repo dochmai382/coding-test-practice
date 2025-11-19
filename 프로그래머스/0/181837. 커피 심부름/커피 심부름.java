ced2e3d272ce285fac7930478d96c7292ffe6eb0
Dami Cho <dochmai382@gmail.com>
2025-11-19 15:39:59 +0900

    [level 0] Title: 커피 심부름, Time: 1.07 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -1,14 +1,9 @@
 class Solution {
     public int solution(String[] order) {
-        // 아메4500, 라떼5000
-        int americano = 0;
-        int latte = 0;
-        int anything = 0;
-        for (String o : order) {
-            if (o.contains("americano")) americano++;
-            else if (o.contains("latte")) latte++;
-            else anything++;
+        int answer = 0;
+        for (String o: order) {
+            answer += o.contains("latte") ? 5000 : 4500;
         }
-        return (americano + anything) * 4500 + latte * 5000;
+        return answer;
     }
 }
