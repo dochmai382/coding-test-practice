58826aceaa398580f5641166b955c57cb688b23d
Dami Cho <dochmai382@gmail.com>
2025-11-20 14:19:23 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.99 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        List<int[]> list = new ArrayList<>();
+        
+        for (int i = 0; i < attendance.length; i++) {
+            if (attendance[i]) {
+                list.add(new int[]{rank[i], i});
+            }
+        }
+        
+        Collections.sort(list, (a, b) -> a[0] - b[0]);
+        
+        int a = list.get(0)[1];
+        int b = list.get(1)[1];
+        int c = list.get(2)[1];
+        
+        return 10000 * a + 100 * b + c;
+    }
+}
