ce1234366f0cc9b17d83ec509e0907bec27a36c5
Dami Cho <dochmai382@gmail.com>
2025-11-20 15:58:09 +0900

    [level 0] Title: 배열의 유사도, Time: 0.13 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        
+        Set<String> setS1 = new HashSet<>(Arrays.asList(s1));
+        
+        for (String s : s2) {
+            if (setS1.contains(s)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
