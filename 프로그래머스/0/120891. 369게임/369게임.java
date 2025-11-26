c3e882700ea6c81791d3c918cde06ff7a2bee0ef
Dami Cho <dochmai382@gmail.com>
2025-11-26 17:08:03 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        while (order > 0) {
+            int remainder = order % 10;
+            if (remainder != 0 && remainder % 3 == 0) answer++;
+            order /= 10;
+        }
+        return answer;
+    }
+}
