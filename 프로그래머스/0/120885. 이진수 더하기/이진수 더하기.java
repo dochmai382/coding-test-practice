d9335edd0af56736c5f4961c5ff111b2133385e4
Dami Cho <dochmai382@gmail.com>
2025-11-27 17:10:01 +0900

    [level 0] Title: 이진수 더하기, Time: 0.06 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,10 +1,37 @@
 class Solution {
     public String solution(String bin1, String bin2) {
-        int dec1 = Integer.parseInt(bin1, 2);
-        int dec2 = Integer.parseInt(bin2, 2);
+        // int dec1 = Integer.parseInt(bin1, 2);
+        // int dec2 = Integer.parseInt(bin2, 2);
+        int dec1 = binaryToDecimal(bin1);
+        int dec2 = binaryToDecimal(bin2);
         
         int sum = dec1 + dec2;
         
-        return Integer.toBinaryString(sum);
+        // return Integer.toBinaryString(sum);
+        return decimalToBinary(sum);
+    }
+    
+    private int binaryToDecimal(String bin) {
+        int decimal = 0;
+        int power = 1;
+        
+        for (int i = bin.length() - 1; i >= 0; i--) {
+            if (bin.charAt(i) == '1') decimal += power;
+            power *= 2;
+        }
+        
+        return decimal;
+    }
+    
+    private String decimalToBinary(int decimal) {
+        if (decimal == 0) return "0";
+        
+        StringBuilder sb = new StringBuilder();
+        while (decimal > 0) {
+            sb.append(decimal % 2);
+            decimal /= 2;
+        }
+        
+        return sb.reverse().toString();
     }
 }
