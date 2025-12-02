8dcc6b8f205aad691df701f83b8174bd4c408640
Dami Cho <dochmai382@gmail.com>
2025-12-02 15:18:49 +0900

    [level 0] Title: 외계어 사전, Time: 0.60 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -3,12 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(String[] spell, String[] dic) {
         Arrays.sort(spell);
-        String target = String.join("", spell);
         
         for (String word: dic) {
-            char[] charArr = word.toCharArray();
-            Arrays.sort(charArr);
-            if (target.equals(new String(charArr))) {
+            String[] strArr = word.split("");
+            Arrays.sort(strArr);
+            if (Arrays.equals(spell, strArr)) {
                 return 1;
             }
         }
