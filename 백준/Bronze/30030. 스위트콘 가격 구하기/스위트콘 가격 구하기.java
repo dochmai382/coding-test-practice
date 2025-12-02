5cf706c427bbd5546fc2e6526d2633108c2e1068
Dami Cho <dochmai382@gmail.com>
2025-12-02 20:21:17 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 168 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int B = sc.nextInt();
+        int A = B * 10 / 11;
+        
+        System.out.println(A);
+    }
+}
