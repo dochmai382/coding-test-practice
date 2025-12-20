61e88f3c0c6308bf53339d1298a582a04dc26fda
Dami Cho <dochmai382@gmail.com>
2025-12-20 19:58:59 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 32.99 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지 - 340212 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212#qna) 
 
 ### 성능 요약
 
-메모리: 120 MB, 시간: 35.75 ms
+메모리: 127 MB, 시간: 32.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 31일 16:30:04
+2025년 12월 20일 19:58:59
 
 ### 문제 설명
 
--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,32 +1,7 @@
 class Solution {
-    private boolean check(long level, long limit, int[] diffs, int[] times) {
-        long totalTime = 0L;
-        int n = diffs.length;
-        
-        totalTime += times[0];
-        
-        for (int i = 1; i < n; i++) {
-            long diff = diffs[i];
-            long timeCur = times[i];
-            long timePrev = times[i - 1];
-            
-            if (diff <= level) {
-                totalTime += timeCur;
-            } else {
-                long k = diff - level;
-                long wrongTime = (timeCur + timePrev) * k;
-                totalTime += wrongTime + timeCur;
-            }
-            
-            if (totalTime > limit) return false;
-        }
-        
-        return totalTime <= limit;
-    }
-    
     public int solution(int[] diffs, int[] times, long limit) {
-        long start = 1L;
-        long end = 100000L;
+        long start = 1;
+        long end = 100000;
         
         int minLevel = (int) end;
         
@@ -42,5 +17,30 @@ class Solution {
         }
         
         return minLevel;
+    }
+    
+    private boolean check(long level, long limit, int[] diffs, int[] times) {
+        long totalTime = 0;
+        int n = diffs.length;
+        
+        totalTime += times[0];
+        
+        for (int i = 1; i < n; i++) {
+            long diff = diffs[i];
+            long currTime = times[i];
+            long prevTime = times[i - 1];
+            
+            if (diff <= level) {
+                totalTime += currTime;
+            } else {
+                long k = diff - level;
+                long wrongTime = (currTime + prevTime) * k;
+                totalTime += wrongTime + currTime;
+            }
+            
+            if (totalTime > limit) return false;
+        }
+        
+        return totalTime <= limit;
     }
 }
