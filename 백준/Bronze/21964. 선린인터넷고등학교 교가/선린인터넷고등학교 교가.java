0e605acb2c0d0810d527270fea1defdd2fb7baf1
Dami Cho <dochmai382@gmail.com>
2025-12-24 19:41:06 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 112 ms, Memory: 15352 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        try (
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            int N = Integer.parseInt(br.readLine());
+            String S = br.readLine();
+
+            String answer = S.substring(N-5, N);
+            bw.write(answer);
+        }
+    }
+}
