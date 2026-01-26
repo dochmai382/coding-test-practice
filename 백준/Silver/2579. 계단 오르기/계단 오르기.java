aadf9a6a560b19018dc9b671778ac51392afaa5f
Dami Cho <dochmai382@gmail.com>
2026-01-26 16:21:04 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -3,47 +3,23 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))){
-
-            // 1. 입력받기 및 준비
-            int N = Integer.parseInt(br.readLine());
+        try (
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+		) {
+            int n = Integer.parseInt(br.readLine());
+            int[] score = new int[n+1];
+            for (int i = 1; i <= n; i++) {
+                score[i] = Integer.parseInt(br.readLine());
+            }
             
-            int[] stairs = new int[N+1];
-            for (int i = 1; i <= N; i++) {
-                stairs[i] = Integer.parseInt(br.readLine());
+            int[] dp = new int[n+1];
+            dp[1] = score[1];
+            if (n >= 2) dp[2] = score[1] + score[2];
+            for (int i = 3; i <= n; i++) {
+                dp[i] = Math.max(dp[i-2] + score[i], dp[i-3] + score[i-1] + score[i]);
             }
-
-            // 각 계단까지의 최대 점수를 저장할 배열 만들기
-            int[] max = new int[N+1];
             
-            // 2. 초기값 설정
-            max[1] = stairs[1];
-
-            // (계단이 2개 이상일 경우) 2번 계단까지의 최대 점수
-            if (N >= 2) {
-                max[2] = stairs[1] + stairs[2];
-            }
-
-            // 3. 점화식 적용 (최대 점수 계산)
-            // 3번 계단부터 마지막 계단까지 반복
-            for (int i = 3; i <= N; i++) {
-                // i번째 계단으로 오는 두 가지 경로의 점수를 계산
-        
-                // 경로 1: i-2번 계단에서 두 칸 점프해서 도착한 경우
-                // (i-2번 계단까지의 최대 점수)
-                int path1 = max[i-2];
-                                
-                // 경로 2: i-1번 계단에서 한 칸 걸어와서 도착한 경우
-                // (i-3번까지의 최대 점수 + i-1번 계단 점수)
-                int path2 = max[i-3] + stairs[i-1];
-                                
-                // 두 경로 중 더 높은 점수를 선택하고, 현재 계단 점수를 더한다
-                max[i] = Math.max(path1, path2) + stairs[i];
-            }
-
-            // 4. 최종결과 출력
-            bw.write(String.valueOf(max[N]));          
+            System.out.println(dp[n]);
         }
     }
 }
