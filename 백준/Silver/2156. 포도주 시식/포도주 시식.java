dc61aafd0aba69fa660e252f9da89cf0f1bcb5cf
Dami Cho <dochmai382@gmail.com>
2026-01-26 15:47:22 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 15032 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = Integer.parseInt(br.readLine());
+            int[] wine = new int[N+1];
+            for (int i = 1; i <= N; i++) {
+                wine[i] = Integer.parseInt(br.readLine());
+            }
+            
+            int[] dp = new int[N+1];
+            dp[1] = wine[1];
+            if (N >= 2) dp[2] = wine[1] + wine[2];
+            if (N >= 3) dp[3] = Math.max(dp[2], 
+                                         Math.max(wine[1] + wine[3], wine[2] + wine[3]));
+            for (int i = 4; i <= N; i++) {
+                dp[i] = Math.max(dp[i-1], 
+                                 Math.max(dp[i-2] + wine[i], dp[i-3] + wine[i - 1] + wine[i]));
+            }
+            
+            System.out.println(dp[N]);
+        }
+    }
+}
