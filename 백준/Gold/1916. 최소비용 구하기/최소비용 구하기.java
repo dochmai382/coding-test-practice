9fb22d15e20008ea66bd1bffba069785d8b0aaa1
Dami Cho <dochmai382@gmail.com>
2026-02-02 15:15:56 +0900

    [Gold V] Title: 최소비용 구하기, Time: 484 ms, Memory: 51952 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,92 +1,89 @@
 import java.util.*;
 import java.io.*;
 
-class Edge implements Comparable<Edge> {
-    private int to;
-    private long cost;
-
-    public Edge(int to, long cost) {
+class Edge {
+    int to;
+    long cost;
+    
+    Edge(int to, long cost) {
         this.to = to;
         this.cost = cost;
-    }
-
-    public int getTo() { return this.to; }
-    public long getCost() { return this.cost; }
-
-    @Override
-    public int compareTo(Edge other) {
-        return Long.compare(this.cost, other.cost);
     }
 }
 
 class Solution {
-
-    public long solve(int N, List<List<Edge>> graph, int startCity, int endCity) {
+    int N, M;
+    int start, end;
+    List<List<Edge>> graph;
+    
+    final long INF = Long.MAX_VALUE;
+    
+    public void input(BufferedReader br) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
         
-        final long INF = Long.MAX_VALUE;
-        long[] dist = new long[N+1];
-        Arrays.fill(dist, INF);
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        dist[startCity] = 0;
-        pq.add(new Edge(startCity, 0));
-
-        while(!pq.isEmpty()) {
-            Edge current = pq.poll();
-            int currentCity = current.getTo();
-            long currentCost = current.getCost();
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        StringTokenizer st;
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
             
-            if (currentCost > dist[currentCity]) {
-                continue;
-            }
-
-            for (Edge next : graph.get(currentCity)) {
-                int nextCity = next.getTo();
-                long edgeWeight = next.getCost();
-
-                long newCost = currentCost + edgeWeight;
-
+            graph.get(s).add(new Edge(e, c));
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+    }
+    
+    private long solve() {
+        long[] dist = new long[N + 1];
+        Arrays.fill(dist, INF);
+        
+        PriorityQueue<Edge> pq = new PriorityQueue<>((e1, e2) -> Long.compare(e1.cost, e2.cost));
+        dist[start] = 0;
+        pq.add(new Edge(start, 0));
+        
+        while (!pq.isEmpty()) {
+            Edge curr = pq.poll();
+            int currCity = curr.to;
+            long currCost = curr.cost;
+            
+            if (currCost > dist[currCity]) continue;
+            
+            for (Edge next : graph.get(currCity)) {
+                int nextCity = next.to;
+                long nextCost = next.cost;
+                
+                long newCost = currCost + nextCost;
                 if (newCost < dist[nextCity]) {
                     dist[nextCity] = newCost;
                     pq.add(new Edge(nextCity, newCost));
                 }
             }
         }
-
-        return dist[endCity];
-    }   
+        
+        return dist[end];
+    }
+    
+    public String getResult() {
+        return String.valueOf(solve());
+    }
 }
 
 class Main {
     public static void main(String[] args) throws IOException {
-        try (
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))
-        ) {
-            int N = Integer.parseInt(br.readLine());
-            int M = Integer.parseInt(br.readLine());
-
-            List<List<Edge>> graph = new ArrayList<>();
-            for (int i = 0; i <= N; i++) {
-                graph.add(new ArrayList<>());
-            }
-
-            StringTokenizer st;
-            for (int i = 0; i < M; i++) {
-                st = new StringTokenizer(br.readLine());
-                int start = Integer.parseInt(st.nextToken());
-                int end = Integer.parseInt(st.nextToken());
-                int weight = Integer.parseInt(st.nextToken());
-                
-                graph.get(start).add(new Edge(end, weight));
-            }
-
-            st = new StringTokenizer(br.readLine());
-            int startCity = Integer.parseInt(st.nextToken());
-            int endCity = Integer.parseInt(st.nextToken());
-
-            Solution sol = new Solution();
-            bw.write(String.valueOf(sol.solve(N, graph, startCity, endCity)));
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        Solution sol = new Solution();
+        sol.input(br);
+        System.out.println(sol.getResult());
     }
 }
+
