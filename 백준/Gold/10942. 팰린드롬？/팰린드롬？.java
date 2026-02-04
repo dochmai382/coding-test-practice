72a5e5d4a605e6bb68e7116db41ba6100fddbec4
Dami Cho <dochmai382@gmail.com>
2026-02-04 16:53:01 +0900

    [Gold IV] Title: 팰린드롬?, Time: 592 ms, Memory: 218192 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -2,36 +2,41 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int[] arr;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int N = Integer.parseInt(br.readLine());
-        arr = new int[N + 1];
+        int[] arr = new int[N + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        boolean[][] dp = new boolean[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) dp[i][i] = true;
+        for (int i = 1; i < N; i++) {
+            if (arr[i] == arr[i+1]) dp[i][i + 1] = true;
+        }
+        for (int len = 2; len < N; len++) {
+            for (int s = 1; s <= N - len; s++) {
+                int e = s + len;
+                if (arr[s] == arr[e] && dp[s+1][e-1]) dp[s][e] = true;
+            }
+        }
+        
         int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
 
-            sb.append(isPell(S, E)).append("\n");
+            if (dp[S][E]) sb.append("1\n");
+            else sb.append("0\n");
         }
 
         System.out.print(sb);
-    }
-
-    private static int isPell(int s, int e) {
-        while (s < e) {
-            if (arr[s++] != arr[e--]) return 0;
-        }
-        return 1;
     }
 }
