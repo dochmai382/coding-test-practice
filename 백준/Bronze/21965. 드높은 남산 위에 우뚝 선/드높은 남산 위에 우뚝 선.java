734870cacd33b8186380ba906fece339202c2264
Dami Cho <dochmai382@gmail.com>
2026-02-14 16:17:44 +0900

    [Bronze I] Title: 드높은 남산 위에 우뚝 선, Time: 256 ms, Memory: 25176 KB -BaekjoonHub

---
+++ 백준/Bronze/21965. 드높은 남산 위에 우뚝 선/드높은 남산 위에 우뚝 선.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean isIncreasing = true;
+        boolean isMountain = true;
+
+        for (int i = 0; i < N - 1; i++) {
+            if (arr[i] < arr[i + 1]) {
+                if (!isIncreasing) {
+                    isMountain = false;
+                    break;
+                }
+            } else if (arr[i] > arr[i + 1]) {
+                isIncreasing = false;
+            } else {
+                isMountain = false;
+                break;
+            }
+        }
+
+        System.out.println(isMountain ? "YES" : "NO");
+    }
+}
