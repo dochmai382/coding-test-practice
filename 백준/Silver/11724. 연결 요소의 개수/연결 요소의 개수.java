f3ad3f2ee89e344cabe41c182a519ff2b1b71976
Dami Cho <dochmai382@gmail.com>
2026-02-19 20:37:39 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 616 ms, Memory: 142144 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    static ArrayList<Integer>[] adj;
+    static boolean[] visited;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        adj = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static void dfs(int node) {
+        if (visited[node]) return;
+
+        visited[node] = true;
+
+        for (int next: adj[node]) {
+            if (!visited[next]) dfs(next);
+        }
+    }
+}
