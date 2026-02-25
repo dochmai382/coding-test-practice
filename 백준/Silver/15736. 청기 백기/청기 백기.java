74d592f2008e54912cef5f4981d75de70a2e6ba7
Dami Cho <dochmai382@gmail.com>
2026-02-25 15:37:58 +0900

    [Silver IV] Title: 청기 백기, Time: 176 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Silver/15736. 청기 백기/청기 백기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long N = scan.nextLong();
+        
+        System.out.println((long) Math.sqrt(N));
+    }
+}
