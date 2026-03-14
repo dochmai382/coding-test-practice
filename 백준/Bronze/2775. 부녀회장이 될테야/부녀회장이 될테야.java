7d4985996ce0933a36f82afbba5003a64db8ef5b
Dami Cho <dochmai382@gmail.com>
2026-03-14 20:45:07 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[][] apt = new int[15][15];
+        for (int i = 0; i < 15; i++) {
+            apt[i][1] = 1;
+            apt[0][i] = i;
+        }
+
+        for (int i = 1; i < 15; i++) {
+            for (int j = 2; j < 15; j++) {
+                apt[i][j] = apt[i][j-1] + apt[i-1][j];
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+            sb.append(apt[k][n]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
