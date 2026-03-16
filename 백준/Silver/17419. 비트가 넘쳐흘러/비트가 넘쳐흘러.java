075650ca61c187bd2695172b6f3c53e12357c4e8
Dami Cho <dochmai382@gmail.com>
2026-03-16 21:21:04 +0900

    [Silver IV] Title: 비트가 넘쳐흘러, Time: 216 ms, Memory: 20040 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/17419. 비트가 넘쳐흘러/비트가 넘쳐흘러.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (input.charAt(i) == '1') count++;
+        }
+
+        System.out.println(count);
+    }
+}
