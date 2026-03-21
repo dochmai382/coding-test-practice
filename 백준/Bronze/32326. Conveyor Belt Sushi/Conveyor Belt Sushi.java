53f7bae2f0c552eaaf206d9a137642c2158f6fed
Dami Cho <dochmai382@gmail.com>
2026-03-21 22:07:09 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 104 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int red = Integer.parseInt(br.readLine());
+        int green = Integer.parseInt(br.readLine());
+        int blue = Integer.parseInt(br.readLine());
+
+        System.out.println(red * 3 + green * 4 + blue * 5);
+    }
+}
