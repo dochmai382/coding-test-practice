1e21ec0f373ceab19599ac2be6d8da1ef382862a
Dami Cho <dochmai382@gmail.com>
2026-03-27 18:49:18 +0900

    [Bronze II] Title: 분해합, Time: 128 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for (int i = 1; i < N; i++) {
+            int num = i;
+            int sum = 0;
+
+            while(num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if (sum + i == N) {
+                result = i;
+                break;
+            }
+        }
+
+        System.out.println(result);
+        
+    }
+}
