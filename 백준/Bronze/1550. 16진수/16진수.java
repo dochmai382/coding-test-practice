c467ab2c7de6b1e27c5ce916786e2f1fbe8c3c9c
Dami Cho <dochmai382@gmail.com>
2026-04-24 20:50:46 +0900

    [Bronze II] Title: 16진수, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/1550. 16진수/16진수.java
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -6,17 +6,6 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String hex = br.readLine();
-
-        int decimal = 0;
-        for (char c: hex.toCharArray()) {
-            int value;
-
-            if ('0' <= c && c <= '9') value = c - '0';
-            else value = c - 'A' + 10;
-
-            decimal = decimal * 16 + value;
-        }
-
-        System.out.println(decimal);
+        System.out.println(Integer.parseInt(hex, 16));
     }
 }
