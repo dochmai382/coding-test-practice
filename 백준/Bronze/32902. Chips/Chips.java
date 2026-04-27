e308bee8fb36004f8727a72a1a830e937f7c82e3
Dami Cho <dochmai382@gmail.com>
2026-04-27 19:53:55 +0900

    [Bronze IV] Title: Chips, Time: 108 ms, Memory: 14500 KB -BaekjoonHub

---
+++ 백준/Bronze/32902. Chips/Chips.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int k = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        System.out.printf("%d %d\n", n + 1, k * n + 1);
+    }
+}
